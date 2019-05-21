7aa482a14fa781d13bdf6d8b2022fae009d1f76d
Raphael Smadja <raphael_smadja@hotmail.com>
2019-05-21 15:11:11 +0200

    button

--- src/app/pokemon.service.ts
+++ src/app/pokemon.service.ts
@@ -11,22 +11,15 @@ export class PokemonService {
 
   goTOFight(pokemon1: Pokemon, pokemon2: Pokemon): void {
     if (pokemon1.getLife() > 0 && pokemon2.getLife() > 0) {
-      if (pokemon1 === pokemon1.compareSpeedOfPokemons(pokemon2) && this.i === 0) {
-        this.poke1Faster = true;
-      } else {
-        this.poke1Faster = false;
-      }
-      if (this.poke1Faster === true) {
+      if (pokemon1 === pokemon1.compareSpeedOfPokemons(pokemon2)) {
         pokemon1.attack(pokemon2);
-        this.poke1Faster = false;
+        pokemon2.attack(pokemon1);
       } else {
         pokemon2.attack(pokemon1);
-        this.poke1Faster = true;
+        pokemon1.attack(pokemon2);
       }
-      console.log(this.poke1Faster);
-      console.log("pokemon1.life - " +pokemon1.getLife());
-      console.log("pokemon2.life - " +pokemon2.getLife());
-      this.i++;
+      console.log("pokemon1.life - " + pokemon1.getLife());
+      console.log("pokemon2.life - " + pokemon2.getLife());
     }
   }
 }
--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -12,18 +12,21 @@ export class PokemonComponent implements OnInit {
 
   constructor(private pokemonService: PokemonService) { }
 
+  pokemons: [Pokemon];
   pokemon1: Pokemon;
   pokemon2: Pokemon;
-  secondsCounter = interval(1000);
-
+  inter;
   ngOnInit() {
-    this.pokemon1 = new Pokemon('pikachu', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png', 10, 2, 20);
-    this.pokemon2 = new Pokemon('leviathan', 'https://www.pokebip.com/pokedex-images/artworks/130.png', 11, 3, 21);
+    this.pokemon1 = new Pokemon('pikachu', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png', 10, 3, 2110);
+    this.pokemon2 = new Pokemon('leviathan', 'https://www.pokebip.com/pokedex-images/artworks/130.png', 11, 4, 2100);
+    this.pokemons.push(this.pokemon1);
+    this.pokemons.push(this.pokemon2);
   }
 
   fight() {
-    this.secondsCounter.subscribe(n =>
-    this.pokemonService.goTOFight(this.pokemon1, this.pokemon2));
+    this.inter = setInterval(() => { this.pokemonService.goTOFight(this.pokemon1, this.pokemon2); }, 1000);
   }
-
+  pause() {
+    clearInterval(this.inter);
+  }
 }
