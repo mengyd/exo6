833f45fd07682ab85b5f0748f8eff6f41da0e932
Massicilia <massicilia.nedir@outlook.com>
2019-05-21 17:04:55 +0200

    adding jest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,19 +14,19 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  test('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'exo6'`, () => {
+  test(`should have as title 'exo6'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('exo6');
   });
 
-  it('should render title in a h1 tag', () => {
+  test('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
