8d7c43621316b600b7b151d17d26fd860f4274ca
mengyd <yidongmeng1992@gmail.com>
2019-05-22 10:39:16 +0200

    fixed unit-test problem in app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,33 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {PokemonComponent} from './pokemon/pokemon.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        PokemonComponent
       ],
     }).compileComponents();
   }));
 
-  test('should create the app', () => {
+  it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
+  }));
 
-  it(`should have as title 'exo'`, () => {
+  it(`should have as title 'exo'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('exo');
-  });
+  }));
 
-  test('should render title in a h1 tag', () => {
+  it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to exo!');
-  });
+  }));
 });
