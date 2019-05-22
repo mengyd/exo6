589f4d184e3d25d6b34fdec40980768383644e4e
mengyd <yidongmeng1992@gmail.com>
2019-05-22 14:09:30 +0200

    test for pokemon service : clearInterval

--- src/app/pokemon.service.spec.ts
+++ src/app/pokemon.service.spec.ts
@@ -6,6 +6,7 @@ import {Pokemon} from './Pokemon';
 describe('PokemonService', () => {
   const pokemon1 = new Pokemon('pikachu', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png', 10, 3, 20);
   const pokemon2 = new Pokemon('leviathan', 'https://www.pokebip.com/pokedex-images/artworks/130.png', 11, 4, 15);
+
   beforeEach(() => TestBed.configureTestingModule({}));
 
   test('should be created', async (() => {
@@ -23,5 +24,13 @@ describe('PokemonService', () => {
     const service: PokemonService = TestBed.get(PokemonService);
     service.goTOFight(pokemon1, pokemon2, pokemon2, 1000);
     expect(pokemon1.getLife()).toBe(16);
+  }));
+
+  test('should called clearInterval', async(() => {
+    const service: PokemonService = TestBed.get(PokemonService);
+    const diedPokemon = new Pokemon('leviathan', 'https://www.pokebip.com/pokedex-images/artworks/130.png', 11, 4, 0);
+    window.clearInterval = jest.fn();
+    service.goTOFight(pokemon1, diedPokemon, pokemon1, 1000);
+    expect(clearInterval).toHaveBeenCalledWith(1000);
   }));
 });
